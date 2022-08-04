6da2476ba09de9486c1251165d382db37cd8d759
Paul Nizet <polonizet@gmail.com>
2022-08-04 19:10:47 +0200

    can display collectibles & display and move player

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+const gridLine = document.querySelector('.grid')
+
+let cellArray = [];
+
+for (let i = 0; i <= 100; i++) {
+  let elementToAppend = document.createElement("div")
+  elementToAppend.classList.add('cell')
+  elementToAppend.dataset.index = i
+  gridLine.append(elementToAppend)
+  cellArray.push(elementToAppend.dataset.index)
+}
+
 class GameBoard {
   constructor(width, height) {
     this.width = width
@@ -6,7 +18,7 @@ class GameBoard {
     this.cells = this._createCells()
   }
   _createCells() {
-    // iteration 1
+    gridLine
   }
 }
 
@@ -65,11 +77,21 @@ const collectibles = [
 ].map((c) => new Collectible(c))
 
 function distributeCollectibles() {
-  // iteration 2
+  let positionsArray = getRandomSelection(6, cellArray)
+
+  cellArray.forEach((cell) => {
+    for (let position in positionsArray) {
+      if (cell === positionsArray[position]) {
+        let divToUpdate = document.querySelector(`[data-index="${positionsArray[position]}"]`)
+        divToUpdate.classList.add(collectibles[position].className)
+      }
+    }
+  })
 }
 
 function getRandomUnoccupiedCell() {
-  // iteration 3
+  let randomPlayerPosition = Math.floor(Math.random() * 100)
+  return randomPlayerPosition
 }
 
 const player = {
@@ -77,13 +99,150 @@ const player = {
   cell: getRandomUnoccupiedCell(),
   show() {
     // iteration 3
+    let divToUpdate = document.querySelector(`[data-index="${this.cell}"]`)
+    divToUpdate.classList.add(this.className)
   },
   hide() {
     // iteration 3
+    let divToUpdate = document.querySelector(`[data-index="${this.cell}"]`)
+    divToUpdate.classList.remove('player')
   },
   move(direction) {
-    // iteration 3
-  },
+    // let divToUpdate = document.querySelector(`[data-index="${this.cell}"]`)
+    divToRemove = document.querySelector(`[data-index="${this.cell}"]`)
+    if (this.cell >= 10 && this.cell <= 89) {
+      console.log(this.cell)
+      switch (direction) {
+        case 'up':
+
+          this.hide()
+          this.cell -= 10;
+          this.show()
+          // console.log('up')
+          // console.log('player position', this.cell)
+          break
+        case 'down':
+          this.hide()
+          this.cell += 10;
+          this.show()
+          // console.log('down')
+          // console.log('player position', this.cell)
+          break
+        case 'left':
+          this.hide()
+          this.cell -= 1;
+          this.show()
+          // console.log('left')
+          // console.log('player position', this.cell)
+          break
+        case 'right':
+          this.hide()
+          this.cell += 1;
+          this.show()
+          // console.log('right')
+          // console.log('player position', this.cell)
+          break
+      }
+    }
+    else if (this.cell >= 0 && this.cell <= 9) {
+      if (this.cell >= 1) {
+        switch (direction) {
+          case 'down':
+            divDown = document.querySelector(`[data-index="${this.cell + 10}"]`)
+            divToRemove.classList.remove('player')
+            divDown.classList.add('player')
+            this.cell += 10;
+            // console.log('down')
+            // console.log('player position', this.cell)
+            break
+          case 'left':
+            divLeft = document.querySelector(`[data-index="${this.cell - 1}"]`)
+            divToRemove.classList.remove('player')
+            divLeft.classList.add('player')
+            this.cell -= 1;
+            // console.log('left')
+            // console.log('player position', this.cell)
+            break
+          case 'right':
+            divRight = document.querySelector(`[data-index="${this.cell + 1}"]`)
+            divToRemove.classList.remove('player')
+            divRight.classList.add('player')
+            this.cell += 1;
+            // console.log('right')
+            // console.log('player position', this.cell)
+            break
+        }
+      } else {
+        switch (direction) {
+          case 'down':
+            divDown = document.querySelector(`[data-index="${this.cell + 10}"]`)
+            divToRemove.classList.remove('player')
+            divDown.classList.add('player')
+            this.cell += 10;
+            // console.log('down')
+            // console.log('player position', this.cell)
+            break
+          case 'right':
+            divRight = document.querySelector(`[data-index="${this.cell + 1}"]`)
+            divToRemove.classList.remove('player')
+            divRight.classList.add('player')
+            this.cell += 1;
+            // console.log('right')
+            // console.log('player position', this.cell)
+            break
+        }
+      }
+    } else if (this.cell >= 90 && this.cell <= 99) {
+      if (this.cell <= 98) {
+        switch (direction) {
+          case 'up':
+            divUp = document.querySelector(`[data-index="${this.cell - 10}"]`)
+            divToRemove.classList.remove('player')
+            divUp.classList.add('player')
+            this.cell -= 10;
+            // console.log('up')
+            // console.log('player position', this.cell)
+            break
+          case 'left':
+            divLeft = document.querySelector(`[data-index="${this.cell - 1}"]`)
+            divToRemove.classList.remove('player')
+            divLeft.classList.add('player')
+            this.cell -= 1;
+            // console.log('left')
+            // console.log('player position', this.cell)
+            break
+          case 'right':
+            divRight = document.querySelector(`[data-index="${this.cell + 1}"]`)
+            divToRemove.classList.remove('player')
+            divRight.classList.add('player')
+            this.cell += 1;
+            // console.log('right')
+            // console.log('player position', this.cell)
+            break
+        }
+      } else {
+        switch (direction) {
+          case 'up':
+            divUp = document.querySelector(`[data-index="${this.cell - 10}"]`)
+            divToRemove.classList.remove('player')
+            divUp.classList.add('player')
+            this.cell -= 10;
+            // console.log('up')
+            // console.log('player position', this.cell)
+            break
+          case 'left':
+            divLeft = document.querySelector(`[data-index="${this.cell - 1}"]`)
+            divToRemove.classList.remove('player')
+            divLeft.classList.add('player')
+            this.cell -= 1;
+            // console.log('left')
+            // console.log('player position', this.cell)
+            break
+        }
+      }
+    }
+  }
+  ,
   canMove(direction) {
     // hint for iteration 3: make move behaviour conditional
   },
@@ -104,22 +263,26 @@ const game = {
     // iteration 4
   },
   start() {
-    // iteration 2
-    // distribute the items
+    distributeCollectibles();
     // iteration 3
-    // show the player
+
     // iteration 4
     // reset the inventory
     // iteration 5
     // reset the music
   },
 }
+player.show();
 
 const startButton = document.querySelector('button#start')
 startButton.addEventListener('click', () => {
-  // iteration 2
-  // start the game
+  if (!game.isStarted) {
+    game.isStarted = true
+    game.start()
+    player.move()
+  }
 })
+
 
 document.addEventListener('keydown', (event) => {
   if (!game.isStarted) {
